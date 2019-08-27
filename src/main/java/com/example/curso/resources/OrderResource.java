b02b89ac446ae44897700bb54e4a79dc11928e0c
Lukas Rodrigo <lukasrar001@gmail.com>
2019-08-27 20:50:46 -0300

    implamentação do orders

--- src/main/java/com/example/curso/resources/OrderResource.java
+++ src/main/java/com/example/curso/resources/OrderResource.java
@@ -1,3 +1,4 @@
+
 package com.example.curso.resources;
 
 import java.util.List;
@@ -9,26 +10,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.curso.entities.User;
-import com.example.curso.services.UserService;
+import com.example.curso.entities.Order;
+import com.example.curso.services.OrderService;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+@RequestMapping(value = "/orders")
+public class OrderResource {
 	
 	@Autowired
-	private UserService service;
+	private OrderService service;
 	
 	@GetMapping 
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<Order>> findAll(){
 
-		List<User> list = service.findAll();
+		List<Order> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id) {
-		User obj = service.findById(id);
+	public ResponseEntity<Order> findById(@PathVariable Long id) {
+		Order obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 }
