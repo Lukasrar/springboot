98dc8a2a8136fb74d8d9ab5dc030a56ca9a21179
Lukas Rodrigo <lukasrar001@gmail.com>
2019-09-10 20:29:03 -0300

    execption handler delete

--- src/main/java/com/example/curso/resources/exceptions/ResourceExecptionHandler.java
+++ src/main/java/com/example/curso/resources/exceptions/ResourceExecptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.example.curso.exceptions.ResourceNotFoundException;
+import com.example.curso.services.exceptions.DatabaseException;
 
 @ControllerAdvice
 public class ResourceExecptionHandler {
@@ -18,6 +19,14 @@ public class ResourceExecptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
