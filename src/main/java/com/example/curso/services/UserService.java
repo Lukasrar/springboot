15245f80fbd19c74ae7b84c21a358a9ddf1309bd
Lukas Rodrigo <lukasrar001@gmail.com>
2019-09-10 20:18:52 -0300

    execption handler user

--- src/main/java/com/example/curso/services/UserService.java
+++ src/main/java/com/example/curso/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.curso.entities.User;
+import com.example.curso.exceptions.ResourceNotFoundException;
 import com.example.curso.repositories.UserRepository;
 
 @Service
@@ -20,7 +21,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();		
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));	
 	}
 	
 	public User insert(User obj) {
