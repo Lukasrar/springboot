777863d3b8782987f5ff13141e1ad1d422acf938
lukas <lukasrar@hotmail.com>
2019-10-08 20:10:34 -0300

    security config

--- src/main/java/com/example/curso/resources/CategoryResource.java
+++ src/main/java/com/example/curso/resources/CategoryResource.java
@@ -33,6 +33,12 @@ public class CategoryResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
 	@PostMapping
 	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
 		CategoryDTO newDto = service.insert(dto);
